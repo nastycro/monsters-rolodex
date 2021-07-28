f244246756d85119978bb7d081ee51a8189dc275
eli <eli@Ekubans-MacBook-Pro.local>
2021-07-28 11:28:12 +0000

    adding fiiles

---
+++ build/precache-manifest.760a4a707d800da8b3fe8970d63c013b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2aa673dd8745edf742fb7754dd26ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e647afe7bba7a3e4367",
+    "url": "/monsters-rolodex/static/css/main.c1faeaeb.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e647afe7bba7a3e4367",
+    "url": "/monsters-rolodex/static/js/main.1b213539.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
